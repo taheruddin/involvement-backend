89b8d8ca4794732748e43cdcf328b1ec2e7a4b2b
Taher Uddin <ktaheruddin@gmail.com>
2020-09-28 09:20:13 +0300

    added tests for sector validation

--- src/test/java/com/hm/backend/unit/service/ValidationTests.java
+++ src/test/java/com/hm/backend/unit/service/ValidationTests.java
@@ -1,5 +1,7 @@
 package com.hm.backend.unit.service;
 
+import com.hm.backend.Utils;
+import com.hm.backend.dao.Sector;
 import com.hm.backend.dto.InvolvementDto;
 import com.hm.backend.exception.IncorrectFormDataException;
 import com.hm.backend.service.ValidationService;
@@ -7,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -84,5 +87,26 @@ public class ValidationTests {
 
         ValidationService service = new ValidationService();
         service.validate(involvementDto);
+    }
+
+    @Test
+    public void whenAtOneOrSectorHasAtLeastOneChildren_thenThrowException() {
+        Sector leafSector = Utils.getLeafSector();
+        Sector branchSector = Utils.getBranchSector();
+        List<Sector> sectors = Arrays.asList(leafSector, branchSector);
+
+        ValidationService service = new ValidationService();
+        assertThatThrownBy(() -> service.validate(sectors))
+            .isInstanceOf(IncorrectFormDataException.class)
+            .hasMessage("Air is not a selectable sector!");
+    }
+
+    @Test
+    public void whenAllSectorsHasZeroChildren_thenDoNotThrowException() throws IncorrectFormDataException {
+        Sector leafSector = Utils.getLeafSector();
+        List<Sector> sectors = Arrays.asList(leafSector, leafSector);
+
+        ValidationService service = new ValidationService();
+        service.validate(sectors);
     }
 }
